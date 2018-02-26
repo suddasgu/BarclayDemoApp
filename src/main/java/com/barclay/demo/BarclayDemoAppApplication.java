a5ba15fc832a82d1aa391df5a78137ecf59053e0
Angshuman Dey <angsdey2@in.ibm.com>
2018-02-26 14:09:45 +0530

    Project created

---
+++ src/main/java/com/barclay/demo/BarclayDemoAppApplication.java
@@ -0,0 +1,12 @@
+package com.barclay.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarclayDemoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarclayDemoAppApplication.class, args);
+	}
+}
