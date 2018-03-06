620bdbf6c557878cfd96b655b4c2b98bb87d3742
suddasgu@in.ibm.com <suddasgu@in.ibm.com>
2018-03-06 12:25:07 +0530

    comit

--- src/main/java/com/barclay/demo/model/Customer.java
+++ src/main/java/com/barclay/demo/model/Customer.java
@@ -1,15 +1,28 @@
 package com.barclay.demo.model;
 
+import io.swagger.annotations.ApiModelProperty;
+
 import org.hibernate.validator.constraints.NotBlank;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection="CUSTOMER")
 public class Customer {
+	
+	@Id
+	@ApiModelProperty(value="Auto generated ID")
 	private String customerId;
-	@NotBlank
+	
+	@ApiModelProperty(required=true)
+	@NotBlank(message = "Mandatory:Customer First Name")
 	private String firstName;
-	@NotBlank
+	
+	@ApiModelProperty(required=true)
+	@NotBlank(message = "Mandatory:Customer Last Name")
 	private String lastName;
+	
+	@ApiModelProperty(required=true)
+	@NotBlank(message = "Mandatory:Customer Address")
 	private String address;
 
 }
